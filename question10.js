6cccfca4a9d64eeed71fbacba48b0f63d2e0fde7
mdovais <ovaiskhan.me@gmail.com>
2024-02-23 19:11:23 +0530

    question10

--- question10.js
+++ question10.js
@@ -4,7 +4,10 @@
 const userObj = {
     name: "Rahul Kumar",
     age: 20,
-    email: "rahul@connect.me"
+    email: "rahul@connect.me";
+    phone:989356455;
+    address:"bhopal";
+    pincode:462102;
 };
 
 console.log(userObj);
