51dcc99f54274545180194aca8e27e78e69a2b38
mdovais <ovaiskhan.me@gmail.com>
2024-02-23 17:52:25 +0530

    question4

---
+++ question4.js
@@ -0,0 +1,5 @@
+// Use push method to add new data in the array, and log the new length of the array, but without using the .length property
+
+const elements = [1,2,3]
+const length=elements.push(4);
+console.log(elements,length);
