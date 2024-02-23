3155a69b70f30104a26ef7d050b428d7a1968690
mdovais <ovaiskhan.me@gmail.com>
2024-02-23 18:33:22 +0530

    question7

---
+++ question7.js
@@ -0,0 +1,4 @@
+// Use shift method to remove first element in the array, and log the removed element. Also log the length of array after removal of element.
+const elements = [1,2,3]
+const removeElem=elements.shift();
+console.log(elements,removeElem,elements.length);
