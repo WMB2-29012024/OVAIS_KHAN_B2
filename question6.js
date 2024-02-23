b5b6d81e1c34229557aee60d12283870e5a54bf8
mdovais <ovaiskhan.me@gmail.com>
2024-02-23 18:18:46 +0530

    question6

---
+++ question6.js
@@ -0,0 +1,4 @@
+// Use unshift method to add new data in the array, and log the new length of the array, but without using the .length property.
+const elements = [1,2,3]
+const lenght=elements.unshift(0);
+console.log(elements,lenght);
