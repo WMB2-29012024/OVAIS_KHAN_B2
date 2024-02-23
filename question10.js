41741d65b9d6e7b305b3182068d94f9d3e9c7cf2
mdovais <ovaiskhan.me@gmail.com>
2024-02-23 19:10:50 +0530

    question10

---
+++ question10.js
@@ -0,0 +1,10 @@
+// Create an object with following keys = “name”, “age”, “email”, “phone”, “address”, “pincode”. And add their corresponding values, and log the object
+
+
+const userObj = {
+    name: "Rahul Kumar",
+    age: 20,
+    email: "rahul@connect.me"
+};
+
+console.log(userObj);
