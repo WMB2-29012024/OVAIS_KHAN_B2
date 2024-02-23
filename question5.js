fd7db97565f4be24b54bccb50fff0b6d47bd28a5
mdovais <ovaiskhan.me@gmail.com>
2024-02-23 18:04:39 +0530

    question5

---
+++ question5.js
@@ -0,0 +1,4 @@
+// Use pop method to remove last element in the array, and log the removed element. Also log the length after removal of element
+const elements = [1,2,3]
+const remove=elements.pop(3);
+console.log(elements,remove,elements.length);
