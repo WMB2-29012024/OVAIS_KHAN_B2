9c70542937dee7b61609cef826d12e0bdab34972
mdovais <ovaiskhan.me@gmail.com>
2024-02-26 19:28:05 +0530

    question13

--- Question13.js
+++ Question13.js
@@ -7,9 +7,9 @@
 
 const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]    
 
-for(let i= 0 ; i <numArr.length ; i ++){
-    console.log (numArr[i]
-        // ,numArr,);
-  
+for(let i= 0 ; i <numArr.length ; i++){
+    for(let j= i ; j <numArr.length ; j++){
+        console.log (numArr[i])    
+    }
 }
 
