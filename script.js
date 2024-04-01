598be5a6b16a5a03b82c60c1d2f374ed243a78fc
mdovais <ovaiskhan.me@gmail.com>
2024-04-01 19:11:53 +0530

    form text

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("button");
+const inputText = document.querySelector("input")
+const placeHolder = document.querySelector("placeholder")
+const text = document.querySelector("p")
+
+
+button.addEventListener("click"){
+    
+}
