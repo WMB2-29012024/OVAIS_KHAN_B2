a239ed11a45b57930f152dcb3e8e7b7d6065721d
mdovais <ovaiskhan.me@gmail.com>
2024-04-30 02:01:09 +0530

    linkedin profile page

---
+++ script.js
@@ -0,0 +1,6 @@
+editProfile = document.getElementsByClassName("edit-profile");
+background = document.getElementsByClassName("background1");
+
+editProfile.addEventListener("click",function(){
+    
+})
