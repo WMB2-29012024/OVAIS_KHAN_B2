17efa45795c450d182c1a374b8982ec67c3a0322
mdovais <ovaiskhan.me@gmail.com>
2024-04-30 13:29:57 +0530

    figma

--- script.js
+++
@@ -1,6 +0,0 @@
-editProfile = document.getElementsByClassName("edit-profile");
-background = document.getElementsByClassName("background1");
-
-editProfile.addEventListener("click",function(){
-    
-})
