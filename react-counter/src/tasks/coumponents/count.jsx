d7d2870cb67c79447ce7e3db23d5ece64659d8a9
mdovais <ovaiskhan.me@gmail.com>
2024-05-07 02:18:30 +0530

    count button

--- react-counter/src/tasks/coumponents/count.jsx
+++ react-counter/src/tasks/coumponents/count.jsx
@@ -1,19 +1,14 @@
 import { useState } from "react";
+
 const Count = ()=> {
     const [count, setCount] =useState(0);
+
     const CountIncrease = () =>{
        setCount(count+1)
     }
 
-    const CountDecrease = () =>{
-        setCount(count-1) 
-    }
     return(
-    <div>
-        <button onClick={CountIncrease}>Increase</button>
-        <div><h1>{count}</h1></div>
-        <button onClick={CountDecrease}>Decrease</button>
-        </div>
+        <button style={{ fontSize: '2em', color: 'white',backgroundColor: 'grey' }} onClick={CountIncrease}>Click me - {count}</button>
     );
 }
 export default Count;
