779a777d3288f88d9ae1e0eeab510e98d730e4d6
mdovais <ovaiskhan.me@gmail.com>
2024-05-06 16:03:18 +0530

    react-Userinfo

---
+++ react-app-user-info/src/tasks/components/UserInfo.jsx
@@ -0,0 +1,7 @@
+const UsrInfo = ({userName}) => {
+      
+        return(
+       <div><h1>Hello <span style={{color: "red"}}>{userName},</span></h1></div>
+        );
+    }
+    export default UsrInfo ; 
---
+++ react-app-user-info/src/tasks/task.jsx
@@ -0,0 +1,8 @@
+import UsrInfo from "./components/UserInfo";
+const Task = () => {
+const userName = "Ovais khan";
+    return(
+        <UsrInfo userName={userName}/>
+    );
+}
+export default Task ;
