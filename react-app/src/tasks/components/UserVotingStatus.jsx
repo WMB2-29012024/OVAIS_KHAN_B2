73ad66b21f0b5249d0e74f2f90ffc96c673672ff
Ovais Khan <157030617+mdovais@users.noreply.github.com>
2024-05-08 03:47:09 +0530

    Update UserVotingStatus.jsx

--- react-app/src/tasks/components/UserVotingStatus.jsx
+++ react-app/src/tasks/components/UserVotingStatus.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const UserVotingStatus = ({ age }) => {
   return (
-    <div>{age===18 ? <p>You can Vote</p> : <p>You can not Vote.</p>}</div>
+    <div>{age===18 ? <p>You can Vote</p> : <p>Your age is below 18. You can't vote now.</p>}</div>
   );
 };
 export default UserVotingStatus;
